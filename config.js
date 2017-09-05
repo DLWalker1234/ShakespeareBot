2ba97df471b395bad60a769fd67a2c543798ff0e
Dustin Walker <thewalkman1234@gmail.com>
2017-09-04 19:43:39 -0500

    before heroku

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: 'l2pUhbgC5rLUypfyxa4XbpbBq',
+    consumer_secret: '2sRctiwo8Jzz5rbU0R6fybONCAjKkgE8gtNyUftY2al5RKY2LT',
+    access_token: '902345246532673536-17SHoe5YPVSdK0vhQIIGFyRHeHlSkBv',
+    access_token_secret: 'AkJz0gVsZ4MPCFiF5oCMxklMAqoMeyoE7dYidY0qvUnDT',
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests. 
+}
--- twitter_bot.js
+++ twitter_bot.js
@@ -1,30 +1,31 @@
 const Twit = require('twit');
 const config = require('./config');
+const userID = '25073877';
 
-console.log(require('shakespeare-insult').random());
 console.log(require('shakespeare-insult').random());
 
 
 var T = new Twit(config);
 
-// var params = {
-//     q: '@realDonaldTrump',
-//     count: 5
-// };
+var params = {
+    q: '@realDonaldTrump',
+    count: 5
+};
 
-// T.get('search/tweets', params, gotData);
+T.get('search/tweets', params, gotData);
 
-// function gotData(err, data, response) {
-//     var tweets = data.statuses;
-//     for (let i = 0; i < tweets.length; i++) {
-//         // console.log(tweets[i].text);
-//     }
+function gotData(err, data, response) {
+    var tweets = data.statuses;
+    for (let i = 0; i < tweets.length; i++) {
+        // console.log(tweets[i].text);
+    }
 
-// };
+};
+
 function dumbTweet() {
     let insult = require('shakespeare-insult').random();
     var tweet = {
-        status: insult
+        status: '@realDonaldTrump ' + insult
     };
 
     T.post('statuses/update', tweet, tweeted);
@@ -33,3 +34,22 @@ function dumbTweet() {
         console.log(data)
     }
 };
+dumbTweet();
+
+
+
+
+// open a stream following events from that user ID
+var stream = T.stream('statuses/filter', { follow: (userID) });
+
+stream.on('tweet', function(tweet) {
+    // compare the user ID inside the Tweet object we passed in
+    // to check it matches
+    if (tweet.user.id == userID) {
+        console.log("this was sent by the user we want to track")
+        dumbTweet();
+    } else {
+        console.log(tweet.user.id + " - " + tweet.user.screen_name)
+            // so we can ignore it
+    }
+});
